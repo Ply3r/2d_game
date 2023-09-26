6dbe2967b51fbbe3bc1a25a0bad39e5324592edb
Leandro Henrique <leandro.resende@vollsolutions.com.br>
2023-09-25 22:00:24 -0300

    the shot kinda works

--- models/Particle.js
+++ models/Particle.js
@@ -10,8 +10,10 @@ class Particle {
     }
     
     this.image = image;
+    this.start_pos = { ...start_pos }
     this.curr_pos = { ...start_pos };
     this.end_pos = { ...end_pos };
+    this.equation = null;
     this.speed = speed;
     this.size = size
     this.visible = true;
@@ -29,8 +31,8 @@ class Particle {
   }
 
   update() {
-    this.curr_pos.x += this.curr_pos.x < this.end_pos.x ? this.speed : -this.speed;
-    this.curr_pos.y += this.curr_pos.y < this.end_pos.y ? this.speed : -this.speed;
+    if (!this.equation) this.lineEquation();
+    this.updatePosition();
 
     if (Math.abs(this.curr_pos.x - this.end_pos.x) <= this.speed &&
         Math.abs(this.curr_pos.y - this.end_pos.y) <= this.speed) 
@@ -41,11 +43,35 @@ class Particle {
     return this;
   }
 
+  lineEquation() {
+    const slope = (this.start_pos.y - this.end_pos.y) / (this.start_pos.x - this.end_pos.x);
+    const b = this.start_pos.y - slope * this.start_pos.x;
+
+    this.equation = (x) => {
+      const new_y = slope * x + b;
+      
+      return { x: x, y: new_y }
+    }
+  }
+
+  // checkParticleOutOfWorld() {
+  //   return this.curr_pos.x < 0 ||
+  //          this.curr_pos.x > window.innerWidth ||
+  //          this.curr_pos.y < 0 ||
+  //          this.curr_pos.y > window.innerHeight
+  // }
+
+  updatePosition() {
+    if (!this.equation) this.lineEquation();
+    this.curr_pos.x += this.curr_pos.x < this.end_pos.x ? this.speed : -this.speed;
+    const { x, y } = this.equation(this.curr_pos.x)
+    this.curr_pos.x = x;
+    this.curr_pos.y = y;
+  }
+
   isVisible() {
     return this.visible;
   }
-
-  
 }
 
 export default Particle;
