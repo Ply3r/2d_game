3411e08db7754344abc7d0fbcd9f57439ca135ef
Leandro Henrique <leandro.resende@vollsolutions.com.br>
2023-09-27 22:09:42 -0300

    finish ingame interface

--- models/Gun.js
+++ models/Gun.js
@@ -1,21 +1,21 @@
 import Canvas from "./Canvas.js";
-import Particles from "./Particles.js";
+import Particles from "./particles/Particles.js";
 
 class Gun {
   MAGAZINE_SIZE = 30
 
-  constructor() {
+  constructor(image = '../assets/guns/tommy_gun.png') {
     this.total_ammunition = 90
     this.using_ammunition = 30
     this.reloading = false;
-    
+    this.image = image;
   }
 
   draw(player_pos, crosshair_pos) {
     const drawer = Canvas.drawer();
 
     const gun_img = new Image();
-    gun_img.src = '../assets/guns/tommy_gun.png';
+    gun_img.src = this.image;
 
     drawer.setTransform(1, 0, 0, 1, player_pos.x + 40, player_pos.y + 70);
     const angle = Math.atan2(crosshair_pos.y - player_pos.y, crosshair_pos.x - player_pos.x);
@@ -40,10 +40,20 @@ class Gun {
       this.using_ammunition += new_using_ammunition;
 
       setTimeout(() => this.reloading = false, 2000);
+      return;
     }
 
     const position = { x: player_pos.x + 40, y: player_pos.y + 70 };
-    Particles.create(position, mouse_position, 15, null, 10);
+    Particles.create(position, mouse_position, 50, null, 10);
+  }
+
+  attributes() {
+    return {
+      total_ammunition: this.total_ammunition,
+      using_ammunition: this.using_ammunition,
+      reloading: this.reloading,
+      image: this.image
+    }
   }
 }
 
